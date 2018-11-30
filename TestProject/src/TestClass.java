dee39d6ade38e98acd8795862a3702b9644bb780
Administrator <Administrator@192.168.1.105>
2018-11-30 14:52:07 -0600

    Added example of Exception Handling to TestClass()

--- TestProject/src/TestClass.java
+++ TestProject/src/TestClass.java
@@ -4,6 +4,23 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//Exception Handling Example
+		try {
+
+			
+			System.out.println("Say Hello World!");
+					//Code that creates Exception
+					int i = 1/0;
+			System.out.println("End of execution.");
+			
+		}
+		catch(Exception exp) {
+			System.out.println("This is inside a Catch Block");
+			System.out.println(exp.getMessage() );
+			System.out.println("Cause is : "+exp.getCause());
+			exp.printStackTrace();
+		}
+		
 	}
 
 }
