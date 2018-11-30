dcdafea659096c9fd12c2de9d149af4d150c24ac
Administrator <Administrator@192.168.1.105>
2018-11-30 15:09:08 -0600

    Put Exception Handling into demo() function and called it from main()

--- TestProject/src/TestClass.java
+++ TestProject/src/TestClass.java
@@ -4,26 +4,30 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//call the demo()
+		demo();
+	}
+	public static void demo() {
 		//Exception Handling Example
-		try {
+				try {
 
-			
-			System.out.println("Say Hello World!");
-					//Code that creates Exception
-					int i = 1/0;
-			System.out.println("End of execution.");
-			
-		}
-		catch(Exception exp) {
-			System.out.println("This is inside a Catch Block");
-			System.out.println(exp.getMessage() );
-			System.out.println("Cause is : "+exp.getCause());
-			exp.printStackTrace();
-		}
-		
-		finally {
-			System.out.println("I am inside Finally block");
-		}
+					
+					System.out.println("Say Hello World!");
+							//Code that creates Exception
+							int i = 1/0;
+					System.out.println("End of execution.");
+					
+				}
+				catch(Exception exp) {
+					System.out.println("This is inside a Catch Block");
+					System.out.println(exp.getMessage() );
+					System.out.println("Cause is : "+exp.getCause());
+					exp.printStackTrace();
+				}
+				
+				finally {
+					System.out.println("I am inside Finally block");
+				}
 	}
 
 }
